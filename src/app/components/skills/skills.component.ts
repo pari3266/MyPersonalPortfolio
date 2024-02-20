27cc771d8466a321344b0f1508b50a9f85bd0b66
pari3266 <parisatalebi25@gmail.com>
2024-02-20 17:19:56 +0330

    update skills

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -12,6 +12,22 @@ import { ToastModule } from 'primeng/toast';
 })
 export class SkillsComponent {
   skills = [
-    {skillName: 'Html' , value: 80}
+    {skillName: 'Html' , value: 80},
+    {skillName: 'css' , value: 70},
+    {skillName: 'js' , value: 30},
+    {skillName: 'ts' , value: 60},
+    {skillName: 'angular' , value: 70},
+    {skillName: 'Angular material' , value: 80},
+    {skillName: 'Bootstrap' , value: 70},
+    {skillName: 'flex' , value: 80},
+    {skillName: 'Pwa' , value: 60},
+    {skillName: 'Web socket' , value: 50},
+    {skillName: 'Google maps , google chart' , value: 70},
+    {skillName: 'RxJs' , value: 70},
+    {skillName: 'NgRx' , value: 40},
+    {skillName: 'NgXs' , value: 60},
+    {skillName: 'Git' , value: 50},
+    {skillName: 'primeNg' , value: 60},
+    {skillName: 'primeFlex' , value: 60},
   ]
 }
